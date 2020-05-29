ecac003ed77351c64003b8b41e6868cfca8854f2
Anderson Alex Durante <anderson.alexdurante@gmail.com>
2020-05-29 18:42:35 -0300

    Layout do Login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,12 +1,25 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View } from 'react-native'
 
-import styles from './styles'
+import { Container, Title, IceCream, EmailInput, ConfirmButton, ButtonText, Forgot, GuestButton, GuestText } from './styles.js'
+import iceCream from '../../assets/ice-cream.png'
 
 export default function Login() {
     return (
-        <View>
-            <Text>Scoops Ahoy</Text>
-        </View>
+        <Container>
+            <View style={{flexDirection: 'row'}}>
+                <Title>Login</Title>
+                <IceCream source={iceCream} />
+            </View>
+            <EmailInput keyboardType="email-address" placeholder="E-mail"/>
+            <EmailInput secureTextEntry={true} placeholder="Senha"/>
+            <ConfirmButton>
+                <ButtonText>Entrar</ButtonText>
+            </ConfirmButton>
+            <Forgot>Esqueceu sua senha?</Forgot>
+            <GuestButton>
+                <GuestText>Entrar como visitante</GuestText>
+            </GuestButton>
+        </Container>
     )
 }
---
+++ src/pages/Login/styles.js
@@ -0,0 +1,70 @@
+import styled from 'styled-components/native'
+
+export const Container = styled.View`
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+`
+
+export const Title = styled.Text`
+    font-size: 30px;
+    font-weight: bold;
+`
+
+export const IceCream = styled.Image`
+    height: 50px;
+    width: 50px;
+    position: relative;
+    bottom: 12px;
+`
+
+export const EmailInput = styled.TextInput`
+    width: 250px;
+    height: 45px;
+    border: 0.3px solid black;
+    padding: 10px;
+    border-radius: 8px;
+    margin-top: 20px;
+`
+
+export const ConfirmButton = styled.TouchableOpacity`
+    width: 100px;
+    height: 45px;
+    background-color: #5dbcd2;
+    border-radius: 8px;
+    padding: 10px;
+    margin-top: 20px;
+    justify-content: center;
+    align-items: center
+`
+
+export const ButtonText = styled.Text`
+    font-size: 20px;
+    font-weight: bold;
+    color: #fff;
+    align-self: center
+`
+
+export const Forgot = styled.Text`
+    font-size: 15px;
+    color: #d3d3d3;
+    font-weight: 100;
+    font-style: italic;
+    margin-top: 15px;
+    text-decoration: underline
+`
+
+export const GuestButton = styled.TouchableOpacity`
+    width: 250px;
+    height: 45px;
+    background-color: #fdd44b;
+    border-radius: 8px;
+    justify-content: center;
+    align-items: center;
+    top: 150px;
+`
+
+export const GuestText = styled.Text`
+    font-size: 20px;
+    color: #fff
+` 
